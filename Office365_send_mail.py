c13c6e6976eb3c6bc769a7882cea4ea9f0b79e9e
hideboyon <hideboyon@hotmail.com>
2020-06-18 10:32:45 +0900

    2 erros have fixed

--- Office365_send_mail.py
+++ Office365_send_mail.py
@@ -1,17 +1,21 @@
 import smtplib
-from emailmessage import EmailMessage
+from email.message import EmailMessage
 
-#メールのパラメータ入力
+#SMTP接続パラメータ入力
 #Enter SMTP Paramater 
 smtp = smtplib.SMTP('smtp.office365.com',587)
 user = 'x.xxxxxx@xxxx.com'
 password = "xxxxxx"
+
+# メール 送信元・宛先・タイトル作成
+# Define From / To / Subject with messag function
+message = EmailMessage()
 message['From'] = 'x.xxxxxx@xxxxx.com'
 message['To'] = 'x.xxxxxx@xxxxx.com'
-message['Subject'] = xxxxx
+message['Subject'] = 'xxxxx'
 
 #SMTP処理
-#SMTP process
+#Processing SMTP
 smtp.ehlo()
 smtp.starttls()
 smtp.ehlo()
